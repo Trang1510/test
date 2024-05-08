105eaca605d7178f9880bab81e9ce32e4c0146b9
Marijn Kagchelland <ml.kagchelland@gmail.com>
2024-05-08 17:52:56 +0200

    Doxy to API draw

--- swont_ide/Core/ub_lib/API_draw.c
+++ swont_ide/Core/ub_lib/API_draw.c
@@ -30,21 +30,29 @@
   * @brief  Sets text on screen in the wanted colour and font
   * @param  x_lup Start location of the text x-axis
   * @param  y_lup Start location of the text y-axis
-  * @param  color string to be displayed
-  * @param  fontname font name
-  * @param  fontsize size of characters
+  * @param  color String to be displayed
+  * @param  fontname Font name
+  * @param  fontsize Size of characters
   * @param  fontstyle
-  * @param reserved unused
-  * @retval HAL status
+  * @param  reserved Unused
+  * @retval Error code
   */
 int API_draw_text(int x_lup, int y_lup, int color, char* text, char* fontname,
                   int fontsize, int fontstyle, int reserved)
 {
     return 1;
 }
-/*
- * @brief
- */
+/**
+  * @brief  Sets line on screen
+  * @param  x_1 Start location x
+  * @param  y_1 Start location y
+  * @param  x_2 End location x
+  * @param  y2 End location y
+  * @param  color Line colour
+  * @param  weight With of the line in pixels
+  * @param  reserved Unused
+  * @retval Error code
+  */
 int API_draw_line(int x_1, int y_1, int x_2, int y2, int color, int weight, int reserved)
 {
     if(weight <= 0)
@@ -138,18 +146,41 @@ int API_draw_line(int x_1, int y_1, int x_2, int y2, int color, int weight, int 
     return 0;
 }
 
-
+/**
+  * @brief  Sets rectangle on screen
+  * @param  x Start location x
+  * @param  y Start location y
+  * @param  width Width of rectangle
+  * @param  height Height of rectangle
+  * @param  color Colour of rectangle
+  * @param  filled Fill rectangle
+  * @param  reserved0 Unused
+  * @param  reserved1 Unused
+  * @retval Error code
+  */
 int API_draw_rectangle(int x, int y, int width, int height, int color, int filled,
                        int reserved0, int reserved1)
 {
     return 1;
 }
 
+/**
+  * @brief  Sets text on screen in the wanted colour and font
+  * @param  x_lup Start location x
+  * @param  y_lup Start location y
+  * @param  bm_nr Bitmap number
+  * @retval Error code
+  */
 int API_draw_bitmap(int x_lup, int y_lup, int bm_nr)
 {
     return 1;
 }
 
+/**
+  * @brief  Fill screen with a the wanted colour
+  * @param  color Colour of screen
+  * @retval Error code
+  */
 int API_clearscreen(uint8_t color)
 {
     uint16_t xp, yp;
