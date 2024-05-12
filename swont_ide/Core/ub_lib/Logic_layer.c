46459378502b52659382fe02f91fc07119856f3c
jackt <jackt@Jacks_Laptop>
2024-05-12 14:53:57 +0200

    Added Logic Layer code base

---
+++ swont_ide/Core/ub_lib/Logic_layer.c
@@ -0,0 +1,62 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : Logic_layer.c
+  * @brief          : Logic functions for SoftOnt project
+  ******************************************************************************
+  * @attention
+  *
+  * Created on: May 8, 2024
+  *    Author: jackt
+  * test
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+#include "main.h"
+
+#define DEBUG
+
+//array for testing
+char command[] = "rechthoek,10,10,150,100,licht blauw,0";
+
+char* ParsedData[MAX_VARS];
+char* data;
+
+void LL_recieve(void)
+{
+	// Get the first token
+#ifdef DEBUG
+	data = strtok(command, ",");
+#endif
+#ifndef DEBUG
+	data = strtok(input.line_rx_buffer, ",");
+#endif
+
+	// Walk through other tokens
+	int i = 0;
+	while (data != NULL && i < MAX_VARS)
+	{
+		ParsedData[i++] = data;
+	    data = strtok(NULL, ",");
+	}
+
+	//DEBUG CHECK for data inside the parsed array
+	for (int j = 0; j < i; j++)
+	{
+		printf("data bit %d: %s\n", j, ParsedData[j]);
+	}
+
+	// Example of checking values
+	// Assuming the command structure is known
+	if (strcmp(ParsedData[0], "rechthoek") == 0)
+	{
+		//print test
+		printf("banger");
+		volatile int x = atoi(ParsedData[1]);
+		volatile int y = atoi(ParsedData[2]);
+		// Check other values and proceed accordingly
+	}
+	return;
+}
---
+++ swont_ide/Core/ub_lib/Logic_layer.h
@@ -0,0 +1,25 @@
+/*
+ * Logic_layer.h
+ *
+ *  Created on: May 8, 2024
+ *      Author: jackt
+ */
+
+#ifndef UB_LIB_LOGIC_LAYER_H_
+#define UB_LIB_LOGIC_LAYER_H_
+
+
+//--------------------------------------------------------------
+// Includes
+//--------------------------------------------------------------
+#include "main.h"
+
+#define MAX_VARS 10
+
+void LL_recieve(void);
+
+
+
+
+
+#endif /* UB_LIB_LOGIC_LAYER_H_ */
