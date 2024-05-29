3ff7e00a6cc1d2d8e14d3eec055259e2051ea1e5
Trang Do <trang.do@student.hu.nl>
2024-05-29 12:40:35 +0200

    Added loging LL_exe() and optimise color defines

--- swont_ide/Core/ub_lib/Logic_layer.c
+++ swont_ide/Core/ub_lib/Logic_layer.c
@@ -8,7 +8,13 @@
 /******************************************************************************
 *   #defines                                                                  *
 ******************************************************************************/
-
+#define SANITIZE_COLOR(colorTypeReturn) ({                                 \
+    if (colorTypeReturn == CMD_UNKNOWN_COLOR)                              \
+    {                                                                   \
+        LOGE("Color unkown");                                             \
+        return;                                                           \
+    }                                                                   \
+})
 /******************************************************************************
 *   Typedefs                                                                  *
 ******************************************************************************/
@@ -47,18 +53,18 @@ ColorType get_color_type(const char* command)
 {
 	if (strncmp(command, "rood", strlen("rood")) == 0) return VGA_COLOUR_RED;
     if (strncmp(command, "blauw", strlen("blauw")) == 0) return VGA_COLOUR_BLUE;
-    if (strncmp(command, "lichtblauw", strlen("lichtblauw")) == 0) return CMD_UNKNOWN;
+    if (strncmp(command, "lichtblauw", strlen("lichtblauw")) == 0) return CMD_UNKNOWN_COLOR;
     if (strncmp(command, "groen", strlen("groen")) == 0) return VGA_COLOUR_GREEN;
-    if (strncmp(command, "lichtgroen", strlen("lichtgroen")) == 0) return CMD_UNKNOWN;
+    if (strncmp(command, "lichtgroen", strlen("lichtgroen")) == 0) return CMD_UNKNOWN_COLOR;
 	if (strncmp(command, "cyaan", strlen("cyaan")) == 0) return VGA_COLOUR_CYAN;
-    if (strncmp(command, "lichtcyaan", strlen("lichtcyaan")) == 0) return CMD_UNKNOWN;
+    if (strncmp(command, "lichtcyaan", strlen("lichtcyaan")) == 0) return CMD_UNKNOWN_COLOR;
     if (strncmp(command, "zwart", strlen("zwart")) == 0) return VGA_COLOUR_BLACK;
-    if (strncmp(command, "lichtrood", strlen("lichtrood")) == 0) return CMD_UNKNOWN;
+    if (strncmp(command, "lichtrood", strlen("lichtrood")) == 0) return CMD_UNKNOWN_COLOR;
     if (strncmp(command, "magenta", strlen("magenta")) == 0) return VGA_COLOUR_MAGENTA;
-	if (strncmp(command, "lichtmagenta", strlen("lichtmagenta")) == 0) return CMD_UNKNOWN;
-    if (strncmp(command, "bruin", strlen("bruin")) == 0) return CMD_UNKNOWN;
+	if (strncmp(command, "lichtmagenta", strlen("lichtmagenta")) == 0) return CMD_UNKNOWN_COLOR;
+    if (strncmp(command, "bruin", strlen("bruin")) == 0) return CMD_UNKNOWN_COLOR;
     if (strncmp(command, "geel", strlen("geel")) == 0) return VGA_COLOUR_YELLOW;
-    if (strncmp(command, "grijs", strlen("grijs")) == 0) return CMD_UNKNOWN;
+    if (strncmp(command, "grijs", strlen("grijs")) == 0) return CMD_UNKNOWN_COLOR;
     if (strncmp(command, "wit", strlen("wit")) == 0) return VGA_COLOUR_WHITE;
     return CMD_UNKNOWN_COLOR;
 }
@@ -112,18 +118,26 @@ void LL_exec(void)
 {
 	CommandType commandType = get_command_type(ParsedData[0]);
 	DEVLOG("%s",ParsedData[0]);
+	ColorType colorType;
 	DEVLOG("%s",ParsedData[5]);
-	ColorType colorType = get_color_type(ParsedData[5]);
 	    switch (commandType)
 	    {
 	        case CMD_LINE:
+	        	colorType = get_color_type(ParsedData[5]);
+	        	SANITIZE_COLOR(colorType);
+	        	DEVLOG("%d",colorType);
 	            API_draw_line(atoi(ParsedData[1]), atoi(ParsedData[2]), atoi(ParsedData[3]), atoi(ParsedData[4]), colorType, atoi(ParsedData[6]), atoi(ParsedData[7]));
 	            break;
 	        case CMD_RECTANGLE:
+	        	colorType = get_color_type(ParsedData[5]);
+	        	SANITIZE_COLOR(colorType);
+	        	DEVLOG("%d",colorType);
 	            API_draw_rectangle(atoi(ParsedData[1]), atoi(ParsedData[2]), atoi(ParsedData[3]), atoi(ParsedData[4]), colorType, atoi(ParsedData[6]), atoi(ParsedData[7]), atoi(ParsedData[8]));
 	            break;
 	        case CMD_TEXT:
 	        	colorType = get_color_type(ParsedData[3]);
+	        	SANITIZE_COLOR(colorType);
+	        	DEVLOG("%d",colorType);
 	        	API_draw_text(atoi(ParsedData[1]), atoi(ParsedData[2]), colorType, ParsedData[4],(ParsedData[5]), atoi(ParsedData[6]), atoi(ParsedData[7]), atoi(ParsedData[8]));
 	            break;
 	        case CMD_BITMAP:
@@ -131,6 +145,8 @@ void LL_exec(void)
 	            break;
 	        case CMD_CLEAR_SCREEN:
 	        	colorType = get_color_type(ParsedData[1]);
+	        	SANITIZE_COLOR(colorType);
+	        	DEVLOG("%d",colorType);
 	            API_clearscreen(colorType);
 	            break;
 	        case CMD_UNKNOWN:
