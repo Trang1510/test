f85bbe1bacc06e28188db86425866a248380f58c
JackT36 <57877480+JackT36@users.noreply.github.com>
2024-05-29 11:20:23 +0200

    Removed Watchdog
    Watchdog causing issues on different hardware.
    Added rev. nbr to hardinf print

--- swont_ide/Core/Src/stm32f4xx_it.c
+++ swont_ide/Core/Src/stm32f4xx_it.c
@@ -237,7 +237,7 @@ void TIM2_IRQHandler(void)
   */
 void USART2_IRQHandler(void)
 {
-	/* USER CODE BEGIN USART2_IRQn 0 */
+  /* USER CODE BEGIN USART2_IRQn 0 */
 
 	// Read the received character
 	char uart_char = USART2->DR;
@@ -269,11 +269,11 @@ void USART2_IRQHandler(void)
 		}
 	}
 
-	/* USER CODE END USART2_IRQn 0 */
-	HAL_UART_IRQHandler(&huart2);
-	/* USER CODE BEGIN USART2_IRQn 1 */
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_IRQHandler(&huart2);
+  /* USER CODE BEGIN USART2_IRQn 1 */
 
-	/* USER CODE END USART2_IRQn 1 */
+  /* USER CODE END USART2_IRQn 1 */
 }
 
 /**
